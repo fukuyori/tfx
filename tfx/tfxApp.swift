9edd2fc3bd0fcc200a5d1ee6fdaf1fbfc6000677
fukuyori <self@spumoni.org>
2026-04-26 12:13:36 +0900

    Initial Commit

---
+++ tfx/tfxApp.swift
@@ -0,0 +1,17 @@
+//
+//  tfxApp.swift
+//  tfx
+//
+//  Created by 福寄典明 on 2026/04/26.
+//
+
+import SwiftUI
+
+@main
+struct tfxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
